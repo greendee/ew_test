e072bdc131afdadfb8a6b94718fe8680001345af
Arisu Desu <arisu@arisu.me>
2016-06-08 14:32:16 +0300

    initial commit

---
+++ employlist/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+
+from .models import Department, Employee
+
+# Register your models here.
+
+class DepartmentAdmin(admin.ModelAdmin):
+    pass
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Personal info', {'fields': ['first_name', 'last_name', 'patronymic', 'birth_date', 'email']}),
+        ('Employee info', {'fields': ['employ_date', 'dismiss_date', 'department', 'position']}),
+    ]
+    list_display = ('get_name_with_position', 'is_employed_now')
+
+
+admin.site.register(Department, DepartmentAdmin)
+admin.site.register(Employee, EmployeeAdmin)
---
+++ employlist/models.py
@@ -0,0 +1,41 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from datetime import date
+
+# Create your models here.
+
+class Department(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.title
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name  = models.CharField(max_length=255)
+    patronymic = models.CharField(max_length=255, blank=True)
+    birth_date = models.DateField()
+    email      = models.EmailField(blank=True)
+#    phone      = models.PhoneNumberField(blank=True)
+    employ_date  = models.DateField()
+    dismiss_date = models.DateField(null=True, blank=True)
+    department   = models.ForeignKey(Department)
+    position     = models.CharField(max_length=255)
+
+    def get_full_name(self):
+        if self.patronymic:
+            return '%s %s %s' % (self.first_name, self.patronymic, self.last_name)
+        else:
+            return '%s %s' % (self.first_name, self.last_name)
+
+    def get_name_with_position(self):
+        return '%s (%s at %s)' % (self.get_full_name(), self.position, self.department)
+
+    def is_employed_now(self):
+        return (self.dismiss_date is None or self.dismiss_date < date.today())
+
+    is_employed_now.boolean = True
+
+    def __unicode__(self):
+        return self.get_name_with_position()
