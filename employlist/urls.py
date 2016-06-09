1dc6a6eb09b41b655acee016c8a87741cef3fce6
Arisu Desu <arisu@arisu.me>
2016-06-09 15:37:19 +0300

    add detail view

--- employlist/urls.py
+++ employlist/urls.py
@@ -1,7 +1,15 @@
 from django.conf.urls import url
+from django.views.generic import DetailView
 from .views import EmployeeListView
-
+from .models import Employee
 
 urlpatterns = [
-    url(r'^list.html$', EmployeeListView.as_view(), name='list'),
+    url(r'^employees/$', EmployeeListView.as_view(), name='list'),
+    url(r'^employees/(?P<pk>[0-9]+).html',
+        DetailView.as_view(
+            model=Employee, template_name='employlist/detail.html',
+            context_object_name='employee'
+        ),
+        name='detail'
+    ),
 ]
